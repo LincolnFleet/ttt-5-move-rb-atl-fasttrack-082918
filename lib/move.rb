fda0f6cb5add130a34abadcf45293fef1d80ac16
Travis Martin <travisemerymartin@gmail.com>
2018-10-18 15:25:32 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,6 +1,6 @@
 board=[" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
-def display_board(board)
+def display_board(board[])
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
@@ -10,7 +10,7 @@ end
 
 def new_game(answer)
   if answer=="y"
-    display_board(board)
+    display_board(board[])
     puts "X goes first."
   else puts "Well then f*$& off!"
     exit!
